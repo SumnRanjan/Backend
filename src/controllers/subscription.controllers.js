4ace9932bc922463b12ac6dc2e6a661d93872037
Suman Ranjan <sumanranjan17291729@gmail.com>
2025-07-17 11:25:14 +0530

    feat: add validation for channel and subscriber IDs in subscription controllers; update comments for clarity in subscription model

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -52,11 +52,39 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if(!mongoose.isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const subscribers = await Subscription.find(
+        {channel : channelId}
+    ).populate("subscriber", "username fullName avatar")
+
+    const count = subscribers.length;
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {subscribers , count}, "Channel subscribers fetched successfully"))
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if(!mongoose.isValidObjectId(subscriberId)){
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const channels = await Subscription.find(
+        {subscriber : subscriberId}
+    ).populate("channel", "username fullName avatar")
+
+    const count = channels.length;
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {channels , count}, "Subscribed channels fetched successfully"))
 })
 
 export {
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,11 +2,11 @@ import mongoose , { Schema } from "mongoose"
 
 const subscriptionSchema = new Schema({
     subscriber : {
-        type : Schema.Types.ObjectId, // one who is subscribing
+        type : Schema.Types.ObjectId, // the user who is subscribing to a channel
         ref : "User"
     },
     channel : {
-        type : Schema.Types.ObjectId, // one to who subscribe is subscribing
+        type : Schema.Types.ObjectId, // the channel owner (who is being subscribed to).
         ref : "User"
     }
 } , {timestamps : true})
